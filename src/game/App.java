d4419b84ccd7661e1c8705a611bde8ef986bc1bb
JohnTW0 <johnpablooliveira85@gmail.com>
2024-05-27 10:40:49 -0500

    Create a new class

--- src/game/App.java
+++ src/game/App.java
@@ -1,7 +1,11 @@
 package game;
 
+import board.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Position pos = new Position(3, 3);
+        System.out.println(pos);
     }
 }
